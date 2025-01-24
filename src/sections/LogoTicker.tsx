353646a695e50f2e2bfa8b9c7b2ad0075fca4745
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-24 13:32:48 -0600

    Added images, styles, and markup to logo ticker component

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,26 @@
+import acmeLogo from '@/assets/logo-acme.png'
+import quantamLogo from '@/assets/logo-quantum.png'
+import ehcoLogo from '@/assets/logo-echo.png'
+import celestialLogo from '@/assets/logo-celestial.png'
+import pulseLogo from '@/assets/logo-pulse.png'
+import apexLogo from '@/assets/logo-apex.png'
+import Image from 'next/image'
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className='py-8 md:py-12 bg-white'>
+      <div className="container">
+        <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
+        <div className='flex gap-14 flex-none'>
+          <Image className='logo-ticker-image' src={acmeLogo} alt='Acme logo'/>
+          <Image className='logo-ticker-image' src={quantamLogo} alt='Quantam logo'/>
+          <Image className='logo-ticker-image' src={ehcoLogo} alt='Echo logo'/>
+          <Image className='logo-ticker-image' src={celestialLogo} alt='Celestial logo'/>
+          <Image className='logo-ticker-image' src={pulseLogo} alt='Pulse  logo'/>
+          <Image className='logo-ticker-image' src={apexLogo} alt='Apex logo'/>
+        </div>
+        </div>
+      </div>
+    </div>
+  )
 };
