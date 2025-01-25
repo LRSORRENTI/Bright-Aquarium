1fec3ae8f9f87d4a7e6e00620444cc6217677adc
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-25 13:54:36 -0600

    Added setup for testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -66,5 +66,18 @@ const testimonials = [
 ];
 
 export const Testimonials = () => {
-  return null;
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="flex justify-center">
+        <div className="tag">Testimonials</div>
+        </div>
+        <h2 className="section-title mt-5">What our users say</h2>
+        <p className="section-description mt-5">From intuitive design to powerful features,
+          our app has become an essential tool for users 
+          around the world.
+        </p>
+      </div>
+    </section>
+  )
 };
