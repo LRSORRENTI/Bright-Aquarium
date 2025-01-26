6443b46ebcfc0155e964f86d75e48dcf17e9276f
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-26 10:22:26 -0600

    Finished Testimonials markup and styles

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -8,6 +8,7 @@ import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 const testimonials = [
   {
@@ -70,10 +71,37 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
+const TestimonialsColumn = (props: {className?: string; testimonials: typeof testimonials }) => {
+    return (
+      <div className={twMerge(
+      "flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",
+      props.className
+      )}
+>       {props.testimonials.map(({ text, imageSrc, name, username }) => (
+        <div className="card" key={text.length}>
+          <div>{text}</div>
+          <div className="flex items-center gap-2 mt-5">
+            <Image src={imageSrc}
+                   width={40} 
+                   height={40} 
+                   alt={name} 
+                   className="h-10 w-10 rounded-full"/>
+            <div className="flex flex-col">
+              <div className="font-medium tracking-tight leading-5">{name}</div>
+              <div className="leading-5 tracking-tight">{username}</div> 
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+    )
+}
+
 export const Testimonials = () => {
   return (
     <section className="bg-white">
       <div className="container">
+        <div className="section-heading">
         <div className="flex justify-center">
         <div className="tag">Testimonials</div>
         </div>
@@ -82,23 +110,11 @@ export const Testimonials = () => {
           our app has become an essential tool for users 
           around the world.
         </p>
-        <div className="flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
-          {firstColumn.map(({ text, imageSrc, name, username }) => (
-            <div className="card" key={text.length}>
-              <div>{text}</div>
-              <div className="flex items-center gap-2 mt-5">
-                <Image src={imageSrc}
-                       width={40} 
-                       height={40} 
-                       alt={name} 
-                       className="h-10 w-10 rounded-full"/>
-                <div className="flex flex-col">
-                  <div className="font-medium tracking-tight leading-5">{name}</div>
-                  <div className="leading-5 tracking-tight">{username}</div> 
-                </div>
-              </div>
-            </div>
-          ))}
+        </div>
+        <div className="flex justify-center gap-6">
+          <TestimonialsColumn testimonials={firstColumn} />
+          <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
+          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
         </div>
       </div>
     </section>
