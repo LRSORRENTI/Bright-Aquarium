97d04eebff5808c6f89efb318dc6cbd9f4cc5e09
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-27 16:13:26 -0600

    Added hover effect to header links

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -22,11 +22,11 @@ export const Header = () => {
         <Image src={Logo} alt='Saas Logo' height={40} width={40}/>
         <MenuItcon className="h-5 w-5 md:hidden" />
         <nav className='hidden md:flex gap-6 text-black/60 items-center'>
-          <a href="">About</a>
-          <a href="">Features</a>
-          <a href="">Customers</a>
-          <a href="">Updates</a>
-          <a href="">Help</a>
+          <a href="" className='hover:text-black transition-all 3s ease-in-out'>About</a>
+          <a href="" className='hover:text-black transition-all 3s ease-in-out'>Features</a>
+          <a href="" className='hover:text-black transition-all 3s ease-in-out'>Customers</a>
+          <a href="" className='hover:text-black transition-all 3s ease-in-out'>Updates</a>
+          <a href="" className='hover:text-black transition-all 3s ease-in-out'>Help</a>
           <button className="group bg-black px-4 py-2 rounded-lg font-medium inline-flex align-middle justify-center tracking-tight transition-all duration-300">
           <span className="bg-clip-text text-white transition-all duration-300 bg-gradient-to-r from-pink-400 via-yellow-400 to-blue-400 group-hover:text-transparent group-hover:bg-gradient-to-r">
           Get for free
